d0a774d10005c99864aebb44ca3f2bbbc63bd33f
mayursuryawanshi2428 <mayursuryawanshi2428@gmail.com>
2026-07-15 17:16:44 +0530

    Time: 3 ms (60.16%) | Memory: 44.6 MB (50.81%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==1){
+            return true;
+        }
+         StringBuilder sb=new StringBuilder("");
+
+         for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(ch>='a' && ch<='z'){
+                sb.append(ch);
+            }
+            else if(ch>='A' && ch<='Z'){
+                ch=(char)(ch- 'A'+'a');
+                sb.append(ch);
+            }
+            else if(ch>='0' && ch<='9'){
+                sb.append(ch);
+            }
+         }
+
+            int start=0;
+            int end=sb.length()-1;
+
+            while(start<end){
+
+                int schar=sb.charAt(start);
+                int echar=sb.charAt(end);
+
+                if (schar!= echar){
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+          }
+         }
+         
